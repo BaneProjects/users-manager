090ab727fb7448cb8b8975bab26a52e5b6f389e9
Branislav <banea380@gmail.com>
2024-12-11 15:04:42 +0100

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { CoreComponent } from './core/core.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -7,7 +6,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,CoreComponent,FooterComponent],
+  imports: [HeaderComponent,CoreComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
